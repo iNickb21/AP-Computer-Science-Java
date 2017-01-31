8531f024fcb17a5c8e03d442f7712fbd2b02e67c
Nick <inickb21@gmail.com>
2017-01-31 11:04:33 -0500

    Changed size of rectangle

--- Chapter 2/P2_5_IntersectionPrinter.java
+++ Chapter 2/P2_5_IntersectionPrinter.java
@@ -5,11 +5,9 @@ public class P2_5_IntersectionPrinter
     public static void main()
     {
         Rectangle r1 = new Rectangle(100,100,50,50),
-        r2 = new Rectangle(25,50,100,20),
+        r2 = new Rectangle(200,200,100,20),
         r3 = r1.intersection(r2);
 
-        System.out.println(r1);
-        System.out.println(r2);
-        System.out.println(r3);
+        System.out.println(r3); // Negative Height or Width means no intersection
     }
 }
