2607d51954dd03e20eb457c06a36e802d8241820
Nick <inickb21@gmail.com>
2017-01-31 11:04:16 -0500

    New random method

--- Chapter 2/P2_11_LotteryPrinter.java
+++ Chapter 2/P2_11_LotteryPrinter.java
@@ -1,24 +1,16 @@
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 public class P2_11_LotteryPrinter
 {
     public static void main()
     {
-        int maxNum = 49;
-        int totalNum = 6;
-        ArrayList<Integer> possibleNumbers = new ArrayList<Integer>(maxNum);
         Random rand = new Random();
-        
-        for(int i = 1; i <= maxNum; i++)
-            possibleNumbers.add(i);
-        
-        System.out.println("The lottery numbers are: ");
-        for(int i = 1; i <= totalNum; i++)
+        int lotteryNumbers = 6;
+        int lotteryMaxNumber = 49;
+
+        for (int i = 0; i < lotteryNumbers; i++)
         {
-            int numChosen = rand.nextInt(possibleNumbers.size());
-            System.out.print(possibleNumbers.remove(numChosen) + " ");
+            System.out.print(rand.nextInt(lotteryMaxNumber) + " " );
         }
-        System.out.println();
     }
 }
