6150812264766959e8f81836d19c91cdc4cb085c
Nick <inickb21@gmail.com>
2017-01-31 11:04:43 -0500

    Removed spacing

--- Chapter 3/Exercise 21 Olympic Rings/OlympicRingComponent.java
+++ Chapter 3/Exercise 21 Olympic Rings/OlympicRingComponent.java
@@ -10,14 +10,14 @@ public class OlympicRingComponent extends JComponent
         g2.setStroke(new BasicStroke(10));
 
         g.setColor(Color.BLUE);
-        g.drawOval (25,25,150,150);
+        g.drawOval(25,25,150,150);
         g.setColor(Color.YELLOW);
-        g.drawOval (110,100,150,150);
+        g.drawOval(110,100,150,150);
         g.setColor(Color.BLACK);
-        g.drawOval (195,25,150,150);
+        g.drawOval(195,25,150,150);
         g.setColor(Color.GREEN);
-        g.drawOval (280,100,150,150);
+        g.drawOval(280,100,150,150);
         g.setColor(Color.RED);
-        g.drawOval (365,25,150,150);
+        g.drawOval(365,25,150,150);
     }
 }
